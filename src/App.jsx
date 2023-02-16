14cf24b09e1935ac19b3444508a14d4a0f17555a
Laes Uddin <laesuddin@gmail.com>
2023-02-16 10:29:02 +0600

    Add AppBar and Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { Typography, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material/'
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 const App = props => {
   return (
-    <div>
-        <h1>Hello, World!</h1>
-    </div>
+    <>
+        <CssBaseline/> 
+        <AppBar position='relative'>
+            <Toolbar>
+                <PhotoCamera/>
+                <Typography variant='h6'>
+                  Photo Album
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <main>
+          <div>
+            <Container maxWidth="sm">
+              <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+                Photo Album
+              </Typography>
+              <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis expedita voluptates ipsam quas ab repellendus quibusdam doloribus qui fugiat libero!
+              </Typography>
+            </Container>
+          </div>
+        </main>
+    </>
   )
 }
-
-App.propTypes = {}
 
 export default App
